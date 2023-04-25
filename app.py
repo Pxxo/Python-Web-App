e36ed9b3e5a79557af38f977f5269e76999b121c
Pino0202 <pinochi0202@gmail.com>
2023-04-25 16:56:54 +0900

    python weather

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+import requests
+import json
+
+getpos_URL = "https://get.geojs.io/v1/ip/geo.json"
+data = requests.get(getpos_URL).json()
+city = data['city']
+response = requests.get(
+    "https://api.openweathermap.org/data/2.5/weather",
+    params={
+        # 緯度・軽度を指定する場合
+        "lat": data['latitude'],
+        "lon": data['longitude'],
+
+        # 都市名で取得する場合
+        # "q": "tokyo",
+        # apikey
+        "appid": '338d9f6cb5d3c0f7a9c3c5f6912409f6',
+        "units": "metric",
+        "lang": "ja",
+    },
+)
+ret = json.loads(response.text)
+
+temp = ret['main']['temp']
+temp_max = ret['main']['temp_max']
+temp_min = ret['main']['temp_min']
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", temp=temp, temp_max=temp_max, temp_min=temp_min, city=city)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
